ce93a40dc05fb7da7891d461648920a8c3bb33b1
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2023-05-24 11:54:43 +0200

    añadir estructura links conteniendo los nodos y la predicción asociada

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 
-input_file = 'test_1.csv'
-output_file = 'submission_1.csv'
+CSV_FILE = 'test_1.csv'
+OUTPUT_FILE = 'submission_1.csv'
 
 def read_links_from_csv(filename):
     links = []
@@ -9,18 +9,26 @@ def read_links_from_csv(filename):
         reader = csv.reader(file)
         next(reader)  # Skip the header row
         for row in reader:
-            link = row[0]
+            link = {
+                'node1': int(row[0].split('-')[0]),
+                'node2': int(row[0].split('-')[1]),
+                'prediction': predict_link()  # Call the predict_link() function
+            }
             links.append(link)
     return links
+
+def predict_link():
+    # Perform your prediction logic here
+    return True
 
 def save_predictions_to_csv(links, filename):
     with open(filename, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Id', 'Predicted'])
         for link in links:
-            writer.writerow([link, 'False'])
+            writer.writerow([f"{link['node1']}-{link['node2']}", link['prediction']])
 
 # Example usage
-links = read_links_from_csv(input_file)
-save_predictions_to_csv(links, output_file)
+links = read_links_from_csv(CSV_FILE)
+save_predictions_to_csv(links, OUTPUT_FILE)
 
