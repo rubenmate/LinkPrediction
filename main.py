85b58ff333209415b9a22e719980394bc5ec02e5
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2023-05-24 13:11:58 +0200

    primera iteración usando vecinos comunes

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
+import networkx as nx
 import csv
 
-CSV_FILE = 'test_1.csv'
+GEXF_FILE = 'social_network_training_1.gexf'
+INPUT_FILE = 'test_1.csv'
 OUTPUT_FILE = 'submission_1.csv'
 
 def read_links_from_csv(filename):
     links = []
+    G = nx.read_gexf(GEXF_FILE)  # Load the graph from GEXF file
+
     with open(filename, 'r') as file:
         reader = csv.reader(file)
         next(reader)  # Skip the header row
+
         for row in reader:
+            node1 = int(row[0].split('-')[0])
+            node2 = int(row[0].split('-')[1])
             link = {
-                'node1': int(row[0].split('-')[0]),
-                'node2': int(row[0].split('-')[1]),
-                'prediction': predict_link()  # Call the predict_link() function
+                'node1': node1,
+                'node2': node2,
+                'prediction': predict_link(G, node1, node2)  # Call the predict_link() function
             }
             links.append(link)
     return links
 
-def predict_link():
-    # Perform your prediction logic here
-    return True
+def predict_link(G, node1, node2):
+    neighbors1 = set(G.neighbors(str(node1)))
+    neighbors2 = set(G.neighbors(str(node2)))
+
+    common_neighbors = neighbors1.intersection(neighbors2)
+    
+    if len(common_neighbors) > 0:
+        return True  # Predict link exists if common neighbors exist
+
+    return False  # Predict link does not exist if no common neighbors
 
 def save_predictions_to_csv(links, filename):
     with open(filename, 'w', newline='') as file:
@@ -29,6 +43,6 @@ def save_predictions_to_csv(links, filename):
             writer.writerow([f"{link['node1']}-{link['node2']}", link['prediction']])
 
 # Example usage
-links = read_links_from_csv(CSV_FILE)
+links = read_links_from_csv(INPUT_FILE)
 save_predictions_to_csv(links, OUTPUT_FILE)
 
